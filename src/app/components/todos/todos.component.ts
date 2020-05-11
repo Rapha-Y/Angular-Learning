b89420195ea6482dfd4a76cc9534fdc674992017
Rapha-Y <raph.yuki@gmail.com>
2020-05-11 16:26:42 -0300

    added todo list

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../models/Todo';
 
 @Component({
   selector: 'app-todos',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todos.component.css']
 })
 export class TodosComponent implements OnInit {
+  todos:Todo[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        id: 1,
+        title: "Wash the dishes",
+        completed: false
+      },
+      {
+        id: 2,
+        title: "Sweep the floor",
+        completed: true
+      },
+      {
+        id: 3,
+        title: "Dry the dishes",
+        completed: false
+      }
+    ]
   }
-
 }
