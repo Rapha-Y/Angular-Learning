54c8b75a5462d1ae6289117f37b6040b23cb9127
Rapha-Y <raph.yuki@gmail.com>
2020-05-11 16:42:11 -0300

    added css for todo list items

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -15,4 +15,14 @@ export class TodoItemComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  //set dynamic classes
+  setClasses() {
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+
+    return classes;
+  }
+
 }
